a5791b0a7f346cd6616a92d9c887f0468f691da0
Tom de Geus <tom@geus.me>
2020-09-08 17:39:23 +0200

    Adding testcase for xtensor_fixed

--- test/test_sfinae.cpp
+++ test/test_sfinae.cpp
@@ -13,7 +13,7 @@
 #include "gtest/gtest.h"
 #include "xtensor/xtensor.hpp"
 #include "xtensor/xarray.hpp"
-// #include "xtensor/xfixed.hpp"
+#include "xtensor/xfixed.hpp"
 #include "xtensor/xview.hpp"
 
 namespace xt
@@ -39,13 +39,13 @@ namespace xt
         xt::xarray<size_t> a = {{9, 9, 9}, {9, 9, 9}};
         xt::xtensor<size_t, 1> b = {9, 9};
         xt::xtensor<size_t, 2> c = {{9, 9}, {9, 9}};
-        // xt::xtensor_fixed<size_t, xt::xshape<2, 2>> d = {{9, 9}, {9, 9}};
+        xt::xtensor_fixed<size_t, xt::xshape<2, 2>> d = {{9, 9}, {9, 9}};
         auto v = xt::view(c, 0, xt::all());
 
         EXPECT_TRUE(xt::all(xt::equal(sfinae_rank_basic_func(a), 0ul)));
         EXPECT_TRUE(xt::all(xt::equal(sfinae_rank_basic_func(b), 0ul)));
         EXPECT_TRUE(xt::all(xt::equal(sfinae_rank_basic_func(c), 2ul)));
-        // EXPECT_TRUE(xt::all(xt::equal(sfinae_rank_basic_func(d), 2ul)));
+        EXPECT_TRUE(xt::all(xt::equal(sfinae_rank_basic_func(d), 2ul)));
         EXPECT_TRUE(xt::all(xt::equal(sfinae_rank_basic_func(v), 0ul)));
     }
 
@@ -78,13 +78,13 @@ namespace xt
         xt::xarray<size_t> a = {{9, 9, 9}, {9, 9, 9}};
         xt::xtensor<size_t, 1> b = {9, 9};
         xt::xtensor<size_t, 2> c = {{9, 9}, {9, 9}};
-        // xt::xtensor_fixed<size_t, xt::xshape<2, 2>> d = {{9, 9}, {9, 9}};
+        xt::xtensor_fixed<size_t, xt::xshape<2, 2>> d = {{9, 9}, {9, 9}};
         auto v = xt::view(c, 0, xt::all());
 
         EXPECT_TRUE(xt::all(xt::equal(sfinae_rank_func(a), 0ul)));
         EXPECT_TRUE(xt::all(xt::equal(sfinae_rank_func(b), 1ul)));
         EXPECT_TRUE(xt::all(xt::equal(sfinae_rank_func(c), 2ul)));
-        // EXPECT_TRUE(xt::all(xt::equal(sfinae_rank_func(d), 2ul)));
+        EXPECT_TRUE(xt::all(xt::equal(sfinae_rank_func(d), 2ul)));
         EXPECT_TRUE(xt::all(xt::equal(sfinae_rank_func(v), 0ul)));
     }
 }
